0f45e789fa0d9bf46ccabd615aad96774e8aab3f
bolortox <bolortogosboujee@gmail.com>
2024-11-13 13:13:56 +0800

    rendered

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const server = express();
 const PORT = 8000;
 
+server.use(cors());
+
 server.get("/", (request, response) => {
-  response.send("Hello get");
+  response.send(["Billy", "biillll"]);
 });
 
 server.listen(PORT, () => {
