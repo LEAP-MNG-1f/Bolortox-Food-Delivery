0c71486ffcdda0c5a9ef507578ed362c0a04b318
bolortox <bolortogosboujee@gmail.com>
2024-11-22 12:43:50 +0800

    crud

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import HomePage from "./_components/pages/HomePage";
-import MenuPage from "./_components/pages/MenuPage";
 
 export default function Home() {
-
   return (
     <div>
       <HomePage />
