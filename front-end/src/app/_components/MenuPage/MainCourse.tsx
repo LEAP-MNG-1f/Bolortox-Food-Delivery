96077b9a3bba1ada6725c393110bd916a53698e4
Bolortox <bolortogosboujee@gmail.com>
2024-11-25 02:27:50 +0800

    cart file created for food cart

--- front-end/src/app/_components/MenuPage/MainCourse.tsx
+++ front-end/src/app/_components/MenuPage/MainCourse.tsx
@@ -3,23 +3,8 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import Typography from "@mui/material/Typography";
-import { Minus } from "../svg/Minus";
-import { Plus } from "../svg/Plus";
 
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2),
-  },
-  "& .MuiDialogActions-root": {
-    padding: theme.spacing(1),
-  },
-}));
+
 
 export type FoodType = {
   _id: string;
@@ -29,13 +14,7 @@ export type FoodType = {
   price: number;
 };
 
-type CardListProps = {
-  foods: FoodType[];
-  arrayNumber: number[];
-  arrayStr: string[];
-};
-
-export const MainCourse = () => {
+export const MainCourse = ({foods} : {foods : FoodType[]}) => {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -47,6 +26,15 @@ export const MainCourse = () => {
 
   return (
     <div className="w-full flex justify-center">
+      <div className="w-[1200px] flex justify-center">
+          <div>
+        {foods.map((food) => {
+          return (<div  key={food._id}>
+           
+            {food.name}
+          </div>)
+        })}
+      </div>
       <Button variant="outlined" onClick={handleClickOpen}>
         <div className=" w-[282px] h-[256px] flex flex-col items-start">
           <img src="/EggToast.png" alt="" />
@@ -54,72 +42,31 @@ export const MainCourse = () => {
           <p className="font-bold text-green-500">15$</p>
         </div>
       </Button>
-      <BootstrapDialog
-        sx={{
-          "& .MuiDialog-paper": {
-            width: "981px",
-            maxWidth: "100%",
-            height: "564px",
-            maxHeight: "100%",
-          },
-        }}
-        onClose={handleClose}
-        open={open}
-      >
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={(theme) => ({
-            position: "absolute",
-            right: 8,
-            top: 8,
-            color: theme.palette.grey[500],
-          })}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogContent>
-          <div className="flex gap-[33px] items-center">
-            <img
-              className="w-[500px] h-[500px] bg-cover "
-              src="/FoodImage.png"
-              alt=""
-            />
+      <Button variant="outlined" onClick={handleClickOpen}>
+        <div className=" w-[282px] h-[256px] flex flex-col items-start">
+          <img src="/EggToast.png" alt="" />
+          <p className="font-bold text-black">Breakfast</p>
+          <p className="font-bold text-green-500">15$</p>
+        </div>
+      </Button>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        <div className=" w-[282px] h-[256px] flex flex-col items-start">
+          <img src="/EggToast.png" alt="" />
+          <p className="font-bold text-black">Breakfast</p>
+          <p className="font-bold text-green-500">15$</p>
+        </div>
+      </Button>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        <div className=" w-[282px] h-[256px] flex flex-col items-start">
+          <img src="/EggToast.png" alt="" />
+          <p className="font-bold text-black">Breakfast</p>
+          <p className="font-bold text-green-500">15$</p>
+        </div>
+      </Button>
 
-            <div className="flex flex-col gap-8">
-              <div>
-                <p className="font-bold text-2xl">Main Pizza</p>
-                <p className="font-bold text-green-500">34,800₮</p>
-              </div>
-              <div>
-                <p className="font-bold">Орц</p>
-                <div className=" w-[368px] h-[54px] text-center rounded-md bg-[#F6F6F6] text-[#767676]">
-                  <p>Хулуу, төмс, лууван , сонгино, цөцгийн тос, самрын үр </p>
-                </div>
-              </div>
-              <p className="font-bold">Тоо</p>
-              <div className="flex gap-[144px]">
-                <button className="w-[45px] h-[40px] bg-[#18BA51] flex justify-center items-center rounded-xl">
-                  <Minus />
-                </button>
-                <p>1</p>
-                <button className="w-[45px] h-[40px] bg-[#18BA51] flex justify-center items-center rounded-xl">
-                  <Plus />
-                </button>
-              </div>
-              <DialogActions>
-                <Button
-                  autoFocus
-                  onClick={handleClose}
-                  className="!w-[384px] !h-[48px]  !bg-[#18BA51] !rounded-xl !text-white"
-                >
-                  Сагслах
-                </Button>
-              </DialogActions>
-            </div>
-          </div>
-        </DialogContent>
-      </BootstrapDialog>
+      </div>
+    
+      
     </div>
   );
 };
--- front-end/src/app/_components/pages/MenuPage.tsx
+++ front-end/src/app/_components/pages/MenuPage.tsx
@@ -1,159 +1,95 @@
-import { MainCourse } from "../MenuPage/MainCourse";
+import { FoodType, MainCourse } from "../MenuPage/MainCourse";
 
 import { Category } from "../MenuPage/Category";
 
 export default function MenuPage() {
-  const foods = [
+  const foodsMockData : FoodType[] = [
     {
       _id: "1",
       name: "Cheeseburger",
       image: "https://example.com/images/cheeseburger.jpg",
       price: 8.99,
-      ingredient: [
-        "Beef patty",
-        "Cheese",
-        "Lettuce",
-        "Tomato",
-        "Pickles",
-        "Onions",
-        "Ketchup",
-        "Mustard",
-      ],
+      ingredient: 
+      " Beef patty, Cheese, Lettuce, Tomato, Pickles, Onions, Ketchup, Mustard"
+      ,
     },
     {
       _id: "2",
       name: "Margherita Pizza",
       image: "https://example.com/images/margherita-pizza.jpg",
       price: 12.49,
-      ingredient: [
-        "Tomato sauce",
-        "Mozzarella cheese",
-        "Fresh basil",
-        "Olive oil",
-      ],
+      ingredient: 
+      "Tomato sauce, Mozzarella cheese, Fresh basil, Olive oil"
+
+      ,
     },
     {
       _id: "3",
       name: "Spaghetti Bolognese",
       image: "https://example.com/images/spaghetti-bolognese.jpg",
       price: 15.99,
-      ingredient: [
-        "Spaghetti",
-        "Ground beef",
-        "Tomato sauce",
-        "Onion",
-        "Garlic",
-        "Parmesan cheese",
-      ],
+      ingredient: 
+      "Spaghetti, Ground beef, Tomato sauce, Onion, Garlic, Parmesan cheese"
     },
     {
       _id: "4",
       name: "Caesar Salad",
       image: "https://example.com/images/caesar-salad.jpg",
       price: 7.49,
-      ingredient: [
-        "Romaine lettuce",
-        "Caesar dressing",
-        "Croutons",
-        "Parmesan cheese",
-        "Chicken breast (optional)",
-      ],
+      ingredient: 
+       "Romaine lettuce, Caesar dressing, Croutons, Parmesan cheese, Chicken breast (optional)"
     },
     {
       _id: "5",
       name: "Chicken Tacos",
       image: "https://example.com/images/chicken-tacos.jpg",
       price: 9.99,
-      ingredient: [
-        "Grilled chicken",
-        "Taco shell",
-        "Lettuce",
-        "Tomato",
-        "Cheese",
-        "Sour cream",
-        "Salsa",
-      ],
+      ingredient: 
+  "Grilled chicken, Taco shell, Lettuce, Tomato, Cheese, Sour cream, Salsa"
     },
     {
       _id: "6",
       name: "Vegetable Stir-fry",
       image: "https://example.com/images/vegetable-stir-fry.jpg",
       price: 10.49,
-      ingredient: [
-        "Broccoli",
-        "Carrots",
-        "Bell peppers",
-        "Onions",
-        "Soy sauce",
-        "Garlic",
-        "Tofu (optional)",
-        "Rice",
-      ],
+      ingredient: 
+       
+      "Broccoli, Carrots, Bell peppers, Onions, Soy sauce, Garlic, Tofu (optional), Rice"
     },
     {
       _id: "7",
       name: "Grilled Salmon",
       image: "https://example.com/images/grilled-salmon.jpg",
       price: 18.99,
-      ingredient: [
-        "Salmon fillet",
-        "Lemon",
-        "Garlic",
-        "Olive oil",
-        "Herbs (dill, parsley)",
-        "Salt",
-        "Pepper",
-      ],
+      ingredient:"Salmon fillet, Lemon, Garlic, Olive oil, Herbs (dill, parsley), Salt, Pepper"
     },
     {
       _id: "8",
       name: "Buffalo Wings",
       image: "https://example.com/images/buffalo-wings.jpg",
       price: 11.99,
-      ingredient: [
-        "Chicken wings",
-        "Buffalo sauce",
-        "Blue cheese dressing",
-        "Celery",
-        "Carrots",
-      ],
+      ingredient: "Chicken wings, Buffalo sauce, Blue cheese dressing, Celery, Carrots"
     },
     {
       _id: "9",
       name: "Pasta Carbonara",
       image: "https://example.com/images/pasta-carbonara.jpg",
       price: 14.49,
-      ingredient: [
-        "Spaghetti",
-        "Eggs",
-        "Pancetta",
-        "Parmesan cheese",
-        "Black pepper",
-        "Garlic",
-      ],
+      ingredient:"Spaghetti, Eggs, Pancetta, Parmesan cheese, Black pepper, Garlic"
     },
     {
       _id: "10",
       name: "Chocolate Cake",
       image: "https://example.com/images/chocolate-cake.jpg",
       price: 5.99,
-      ingredient: [
-        "Flour",
-        "Cocoa powder",
-        "Sugar",
-        "Eggs",
-        "Butter",
-        "Vanilla extract",
-        "Baking soda",
-        "Milk",
-      ],
+      ingredient:"Flour, Cocoa powder, Sugar, Eggs, Butter, Vanilla extract, Baking soda, Milk"
     },
   ];
 
   return (
     <div>
       <Category />
-      <MainCourse />
+      <MainCourse foods = {foodsMockData}/>
     </div>
   );
 }
