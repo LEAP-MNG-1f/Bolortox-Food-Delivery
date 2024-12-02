cbafb4f8a17d9a2c275b8a80f8432bc19b6bd21f
Bolortox <bolortogosboujee@gmail.com>
2024-12-02 16:23:40 +0800

    home

--- back-end/controllers/food.controller.js
+++ back-end/controllers/food.controller.js
@@ -3,12 +3,12 @@ import { Food } from "../model/food.model.js";
 const createFood = async (request, response) => {
   try {
     const result = await Food.create({
-      name: "Yogurt bowl",
+      name: "Pizza",
       image:
-        "http://res.cloudinary.com/dkm8vqcln/image/upload/v1732477540/m3k0mfbz1xozkoacbhp8.png",
-      ingredient: "Oat, yogurt, berries , banana",
-      price: "9000",
-      categoryId: "674929948d7746ef987104cd",
+       "http://res.cloudinary.com/dkm8vqcln/image/upload/v1732474813/gkt8okbs4idtwakascq6.png",
+      ingredient: " Grounded beef, Cheese, Lettuce, Tomato, Pepperoni, Onions, Tomato sauce, Mustard sauce",
+      price: "28000",
+      categoryId: "67492f896ba784bf4bd9e153",
     });
     response.json({
       success: true,
