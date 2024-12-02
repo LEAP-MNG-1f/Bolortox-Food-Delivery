6609f3fb2a350183466d65d4d21152aed666585b
bolortox <bolortogosboujee@gmail.com>
2024-12-02 12:59:38 +0800

    got foods from backend

--- front-end/src/app/_components/pages/HomePage.tsx
+++ front-end/src/app/_components/pages/HomePage.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import { HomePageBody } from "../HomePage/HomePageBody";
 import { HomePageInfo } from "../HomePage/HomePageInfo";
-import { MainCourse } from "../MenuPage/MainCourse";
+import { FoodType, MainCourse } from "../MenuPage/MainCourse";
 
 export default function HomePage() {
+  const [foodData, setFoodData] = useState([]);
+
+  const getFoodData = async () => {
+    const response = await fetch("http://localhost:8001/api/food");
+    const res = await response.json();
+    setFoodData(res.data);
+  };
+  useEffect(() => {
+    getFoodData();
+  }, []);
+
+  console.log(foodData);
   return (
     <div>
       <HomePageBody />
-
+      <MainCourse foods={foodData} />
       <HomePageInfo />
     </div>
   );
