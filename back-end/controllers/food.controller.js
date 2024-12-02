ba613993b7e3e92c74213cfd2923690f73007ffa
bolortox <bolortogosboujee@gmail.com>
2024-12-02 13:00:09 +0800

    backend fixed

--- back-end/controllers/food.controller.js
+++ back-end/controllers/food.controller.js
@@ -3,12 +3,13 @@ import { Food } from "../model/food.model.js";
 const createFood = async (request, response) => {
   try {
     const result = await Food.create({
-      name: "Yogurt bowl",
+      name: "Oreo Shake",
       image:
-        "http://res.cloudinary.com/dkm8vqcln/image/upload/v1732477540/m3k0mfbz1xozkoacbhp8.png",
-      ingredient: "Oat, yogurt, berries , banana",
-      price: "9000",
-      categoryId: "674929948d7746ef987104cd",
+        "http://res.cloudinary.com/dkm8vqcln/image/upload/v1732477151/nuthws85m7emfmybquzx.png",
+      ingredient:
+        "Oreo , Vanilla sugar, Milk , Ice , Chocolate syrup , Chocolate Vanilla ice cream",
+      price: "89000",
+      categoryId: "67492f896ba784bf4bd9e153",
     });
     response.json({
       success: true,
--- back-end/routes/foodRouter.js
+++ back-end/routes/foodRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createFood } from "../controllers/food.controller.js";
+import { createFood, getAllFood } from "../controllers/food.controller.js";
 
 const foodRouter = express.Router();
 
 foodRouter.post("/food", createFood);
+
+foodRouter.get("/food", getAllFood);
 
 export { foodRouter };
